d773d71bb494c405b53f0ce965e7cf407330921c
ratataca <gywls18@koreatech.ac.kr>
2022-03-12 23:01:30 +0900

    2022. 03. 12.

--- week 10. DP & 파싱(문자열 처리)/백준/1. 2579 계단오르기/ratataca.py
+++ week 10. DP & 파싱(문자열 처리)/백준/1. 2579 계단오르기/ratataca.py
@@ -1,4 +1,3 @@
-### 왜 92% 가다가 틀리는 거지..?
 import sys
 input = sys.stdin.readline
 
@@ -18,20 +17,5 @@ elif N == 2:
 else:
     for i in range(3, N+1):
         memo[i] = max(memo[i-2], stairs[i-1]+memo[i-3]) + stairs[i]
-        print('i: ', i, " | ", memo )
-
+        
     print(memo[-1])
-
-
-# import sys
-# read = sys.stdin.readline
-
-# n = int(read())
-# stairs = [0] + [int(read()) for _ in range(n)] + [0]
-# cache = [0] * (n+2)
-# cache[1] = stairs[1]
-# cache[2] = cache[1] + stairs[2]
-
-# for i in range(3, n+1):
-#     cache[i] = max(cache[i-2], cache[i-3] + stairs[i-1]) + stairs[i]
-# print(cache[n])
